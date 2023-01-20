5388924048ac728c43ba653d40edae03ca1563ef
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2023-01-20 16:17:13 -0300

    Adiciona imagem, textos e botões na página

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ function App(){
 
     return(
         <div>
-
+            <h1>Cronômetro</h1>
+            <img src={require('./assets/cronometro.png')}/>
+            <strong>0.0</strong>
+            <div>
+                <a>INICIAR</a>
+                <a>LIMPAR</a>
+            </div>
         </div>
     );
 }
