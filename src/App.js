5afd5fcdc2429d40cafde7225249046e333698a3
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2023-01-20 16:41:26 -0300

    Adiciona estilo aos elementos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, {useState} from 'react';
+import './estilo.css';
 
 function App(){
 
     return(
-        <div>
+        <div className='container'>
             <h1>Cronômetro</h1>
-            <img src={require('./assets/cronometro.png')}/>
-            <strong>0.0</strong>
-            <div>
-                <a>INICIAR</a>
-                <a>LIMPAR</a>
+            <img src={require('./assets/cronometro.png')} className='img'/>
+            <strong className='timer'>0.0</strong>
+            <div className='acaoBtn'>
+                <a className='botao'>INICIAR</a>
+                <a className='botao'>LIMPAR</a>
             </div>
         </div>
     );
